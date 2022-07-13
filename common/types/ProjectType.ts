14b0bc7a9e1bef305838b8a2487a5972b2353402
Rudy B <btte.rudy@gmail.com>
2022-07-13 18:07:25 +0200

    Fix Icon's import in ProjectType

--- common/types/ProjectType.ts
+++ common/types/ProjectType.ts
@@ -1,15 +1,15 @@
-import { ReactElement } from 'react';
-import Image from 'next/image';
-import { TagType } from './TagType';
-import Icon from '../components/Icon/Icon';
+import { ReactElement } from 'react'
+import Image from 'next/image'
+import { TagType } from './TagType'
+import Icon from '../components/elements/Icon/Icon'
 
 export type ProjectType = {
-  title: string;
-  description: string;
-  demo?: string;
-  github?: string;
-  tags: TagType[];
-  preview: ReactElement<typeof Image>;
-  date: Date;
-  icon: ReactElement<typeof Icon>;
-};
+  title: string
+  description: string
+  demo?: string
+  github?: string
+  tags: TagType[]
+  preview: ReactElement<typeof Image>
+  date: Date
+  icon: ReactElement<typeof Icon>
+}
