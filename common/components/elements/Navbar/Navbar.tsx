5cd1dc000d882ee6d4065e8dcc44f9ed0ee15153
Rudy B <btte.rudy@gmail.com>
2022-07-28 14:47:29 +0200

    Fix navbar background on already scrolled page

--- common/components/elements/Navbar/Navbar.tsx
+++ common/components/elements/Navbar/Navbar.tsx
@@ -14,6 +14,7 @@ const Navbar = () => {
   }
 
   useEffect(() => {
+    listenScrollEvent()
     window.addEventListener('scroll', listenScrollEvent)
     return () => {
       window.removeEventListener('scroll', listenScrollEvent)
