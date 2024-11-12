6a26004fc98f1dfc395b6e3cd1c55f12084aad31
Rudy Boutte <32516879+boutterudy@users.noreply.github.com>
2024-11-12 22:03:22 +0100

    feat(urls): translated pathnames

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -7,6 +7,24 @@ export const routing = defineRouting({
 
   // Used when no locale matches
   defaultLocale: 'en',
+
+  // The `pathnames` object holds pairs of internal and
+  // external paths. Based on the locale, the external
+  // paths are rewritten to the shared, internal ones.
+  pathnames: {
+    // If all locales use the same pathname, a single
+    // external path can be used for all locales
+    '/': '/',
+    '/mes-projets': {
+      fr: '/mes-projets',
+      en: '/my-projects',
+    },
+    '/mon-parcours': {
+      fr: '/mon-parcours',
+      en: '/my-journey',
+    },
+    contact: 'contact',
+  },
 })
 
 // Lightweight wrappers around Next.js' navigation APIs
