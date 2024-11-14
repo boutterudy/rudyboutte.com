594072b33a0750c4cf7bd6b6582f996ba0d98ba8
Rudy Boutte <32516879+boutterudy@users.noreply.github.com>
2024-11-15 00:24:42 +0100

    fix: add `standalone` output

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withNextIntl = createNextIntlPlugin()
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  output: 'standalone',
 }
 
 module.exports = withNextIntl(nextConfig)
